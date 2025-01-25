7c4b192cf7fe54b39b5c2d51e134cb36dfcefc27
X_V.neer <xv.neer@gmail.com>
2025-01-25 13:01:43 +0200

    add import alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import tailwindcss from "@tailwindcss/vite"
+import path from "node:path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+    },
+  },
   plugins: [react(), tailwindcss()],
 })

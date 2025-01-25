98c633a57ada2909107c7d1e7edb5cc1029b8837
X_V.neer <xv.neer@gmail.com>
2025-01-25 12:59:23 +0200

    intilized react router

---
+++ src/router.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router"
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path="/auth" element={<div />} />
+    </Routes>
+  )
+}
+
+export default Router

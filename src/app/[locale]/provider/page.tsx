d78bc2905674f73f62395d9e3d7405f6df2b324b
X_V.neer <xv.neer@gmail.com>
2025-01-31 10:32:32 +0200

    create passengers graph

--- src/app/[locale]/provider/page.tsx
+++ src/app/[locale]/provider/page.tsx
@@ -1,9 +1,66 @@
-import React from "react"
+// import usePermissions from "@/hooks/use-permissions"
+
+import DateRangeInput from "@/components/ui/date-range-input"
+import { BarChart } from "@mantine/charts"
+import { Divider, Group, Paper, Stack, Text } from "@mantine/core"
+
+import { useTranslation } from "react-i18next"
+const dummyData = [
+  { x: '2023-04-01', y: 120 },
+  { x: '2023-04-03', y: 185 },
+  { x: '2023-04-04', y: 210 },
+  { x: '2023-04-05', y: 185 },
+  { x: '2023-04-06', y: 294 },
+  { x: '2023-04-07', y: 156 },
+  { x: '2023-04-08', y: 156 },
+  { x: '2023-04-09', y: 245 },
+  { x: '2023-04-10', y: 294 },
+  { x: '2023-04-11', y: 156 },
+  { x: '2023-04-12', y: 178 },
+  { x: '2023-05-07', y: 156 },
+  { x: '2023-05-08', y: 156 },
+  { x: '2023-05-09', y: 245 },
+  { x: '2023-05-10', y: 294 },
+  { x: '2023-05-11', y: 156 },
+  { x: '2023-05-12', y: 178 },
+]
 
 type Props = {}
 
 const Home = (props: Props) => {
-  return <div>Home</div>
+  const { t } = useTranslation()
+
+  return (
+    <Stack>
+      <Paper component={Stack} gap={"lg"} p="lg" className="grow" radius="md">
+        <Group justify="space-between" py="xs">
+          <Text size="lg" fw={600}>
+            {t("home.passengers-graph.title")}
+          </Text>
+          <Group grow justify="end" className="grow " >
+            <DateRangeInput />
+          </Group>
+        </Group>
+
+        <Divider color="gray.1" />
+        <BarChart
+          withLegend
+          h={290}
+          data={dummyData}
+          dataKey={"x"}
+          series={[{ name: "y", label: t('home.passengers-graph.title'), color: "primary" }]}
+          barProps={{ radius: [10, 10, 0, 0] }}
+          tickLine="y"
+          yAxisLabel={t("home.passengers-graph.y-axis-label")}
+        />
+        <Group justify="center" className="capitalize">
+          <Text fw={500}>
+            {t('general.date')}
+          </Text>
+        </Group>
+      </Paper>
+    </Stack>
+  )
 }
 
 export default Home
---
+++ src/components/ui/date-range-input.tsx
@@ -0,0 +1,41 @@
+import { DatePickerInput, DatePickerInputProps, DatesRangeValue, DateValue } from "@mantine/dates"
+import dayjs from "dayjs"
+import { Calendar } from "lucide-react"
+import { useQueryState } from "nuqs"
+import { useTranslation } from "react-i18next"
+type Props = {
+  queryKey?: string
+} & Omit<DatePickerInputProps<'range'>, "onChange" | "value" | "type">
+
+const DateRangeInput = ({ queryKey, ...props }: Props) => {
+  // Use Nuqs to sync state with URL
+  const [range, setRange] = useQueryState<DatesRangeValue>(queryKey || "date-ange", {
+    defaultValue: [null, null], // Default empty range  
+    parse: (value) => {
+      if (!value) return [null, null] as const
+      const [start, end] = value.split("_")
+      return [start ? dayjs(start).toDate() : null, end ? dayjs(end).toDate() : null] as const
+    },
+    serialize: (value) => {
+      if (!value || !value[0] || !value[1]) return ""
+      return `${dayjs(value[0]).format("YYYY-MM-DD")}_${dayjs(value[1]).format("YYYY-MM-DD")}`
+    },
+  })
+  const { t } = useTranslation()
+  return (
+    <DatePickerInput
+      className="grow shrink-0"
+      type="range"
+      maw={260}
+      w={'100%'}
+      size="md"
+      placeholder={t("general.date-range-input-placeholder")}
+      rightSection={<Calendar className="size-5" />}
+      value={range}
+      onChange={setRange}
+      {...props}
+    />
+  )
+}
+
+export default DateRangeInput
